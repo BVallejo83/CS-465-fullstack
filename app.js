118a296e05e1cdb75193e867f2536ee3d198122a
BVallejo83 <benjamin.vallejovazquez@gmail.com>
2026-03-25 07:53:57 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const path = require('path');
+const hbs = require('hbs');
+require('./app_server/models/db');
+
+const app = express();
+const port = 3000;
+
+// view engine setup
+app.set('views', path.join(__dirname, 'app_server', 'views'));
+app.set("view engine", "hbs");
+
+
+// static files
+app.use(express.static(path.join(__dirname, 'public')));
+
+// routes
+const indexRouter = require('./app_server/routes/index');
+const travelRouter = require('./app_server/routes/travel');
+app.use('/', indexRouter);
+app.use('/travel', travelRouter);
+
+// start server
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
